2a6f157448db3aa3b1828bf3f82f0876d85a07f5
elia barume <eliamurula@gmail.com>
2025-08-06 10:58:32 +0200

    program for checking role for the students and members

---
+++ variable.py
@@ -0,0 +1,31 @@
+name = "john_doe"
+age = 30
+is_active = True
+email = "johndoe@gmail.com"
+roles = ["admin", "user", "editor"]
+permissions = {
+    "read": True,
+    "write": False,
+    "execute": True
+
+}
+settings = {   
+    "theme": "dark",
+    "notifications": True,
+    "language": "en"
+} 
+
+
+user_data = {
+    "name": name,
+    "age": age,
+    "is_active": is_active,
+    "email": email,
+    "roles": roles,
+    "permissions": permissions,
+    "settings": settings
+}
+ 
+
+
+
